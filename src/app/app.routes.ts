14df3b5265769f1ba7e90cb3d574e1265b144eca
Ali Hassan <engineralihassan@gmail.com>
2024-05-31 10:46:28 -0400

    tasks board task finish

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { Testing1Component } from './components/testing1/testing1.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'login', pathMatch:'full'},
@@ -18,7 +17,5 @@ export const routes: Routes = [
       ]
     },
     
-    {path:'testing1', component:Testing1Component},
-    {path:'testing2', loadComponent:() => import('./components/testing2/testing2.component').then((data)=> data.Testing2Component )
-}
+
 ];
--- src/app/components/single-project/single-project.component.ts
+++ src/app/components/single-project/single-project.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { SkeltonLoaderComponent } from '../skelton-loader/skelton-loader.component';
 import { ProjectfiltersComponent } from './components/projectfilters/projectfilters.component';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBook, faEllipsis } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-single-project',
   standalone: true,
-  imports: [CommonModule,SkeltonLoaderComponent,ProjectfiltersComponent],
+  imports: [FontAwesomeModule,CommonModule,SkeltonLoaderComponent,ProjectfiltersComponent],
   templateUrl: './single-project.component.html',
   styleUrl: './single-project.component.css'
 })
 export class SingleProjectComponent {
-
+isLoading=false;
+icons=[faEllipsis,faBook]
+ngOnInit(){
+  // this.isLoading=true;
+  setTimeout(()=>{
+    this.isLoading=false;
+  },5000)
+}
 }
